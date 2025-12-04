7e77713990c2e8ea9b224bde0b43338b733550de
salviega <salviega6@gmail.com>
2025-12-04 15:41:25 -0500

    🔧 Updated configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,68 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-plugin-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts'
+	]),
 
-export default eslintConfig;
+	...nextVitals,
+	...nextTs,
+
+	{
+		files: ['**/*.{mjs,cjs,js,jsx,ts,tsx}'],
+
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				React: 'writable'
+			}
+		},
+
+		plugins: {
+			prettier,
+			'simple-import-sort': simpleImportSort,
+			'unused-imports': unusedImports
+		},
+
+		rules: {
+			// General
+			eqeqeq: ['warn', 'always'],
+
+			'@typescript-eslint/no-unused-vars': 'off',
+
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'warn',
+
+			'unused-imports/no-unused-imports': 'warn',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_'
+				}
+			],
+
+			'prettier/prettier': 'warn',
+
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn'
+		}
+	}
+])
+
+export default eslintConfig
