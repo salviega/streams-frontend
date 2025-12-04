994ce0f4c9e9eccf8b0372a17fbd914c4e3e3f28
salviega <salviega6@gmail.com>
2025-12-04 16:49:26 -0500

    ✨ Added navbar

--- src/app/shared/Navbar/index.tsx
+++ src/app/shared/Navbar/index.tsx
@@ -5,23 +5,53 @@ import { LoginButton } from './_componets/LoginButton'
 
 export default function Navbar(): JSX.Element {
 	return (
-		<header className="w-full flex flex-col p-12 gap-3">
-			<div className="flex flex-row items-center justify-between">
-				<h1 className="text-xl font-semibold tracking-tight md:text-2xl">
-					Streamer plataform 💦
-				</h1>
-
-				<div className="flex flex-row items-center justify-center gap-15">
-					<button type="button">➕ New campaign</button>
-					<button type="button">▶️ Start campaign</button>
+		<div className="navbar bg-base-100 shadow-sm">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							{' '}
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h8m-8 6h16"
+							/>{' '}
+						</svg>
+					</div>
+					<ul
+						tabIndex={-1}
+						className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+					>
+						<li>
+							<button>➕ New campaign</button>
+						</li>
+						{/* <li>
+							<a>Parent</a>
+							<ul className="p-2">
+								<li></li>
+							</ul>
+						</li> */}
+					</ul>
 				</div>
-
+				<a className="btn btn-ghost text-xl"> Streamer plataform 💦</a>
+			</div>
+			<div className="navbar-center hidden lg:flex">
+				<ul className="menu menu-horizontal px-1">
+					<li>
+						<button>➕ New campaign</button>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-end">
 				<LoginButton />
 			</div>
-			<p className="mt-1 max-w-xl text-xs text-slate-400 md:text-sm">
-				Recompensas continuas para atraer LPs a nuevos tokens. Conecta campañas
-				Superfluid GDA + Uniswap v4 desde un solo panel.
-			</p>
-		</header>
+		</div>
 	)
 }
